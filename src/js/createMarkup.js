f0574ff798865e1861930b70538d43e49dd3c1d7
vitaliy-kro <krochakv6@gmail.com>
2022-09-07 20:46:13 +0300

    changing fetch methods to axios

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -5,32 +5,7 @@ import card from '../template/card.handlebars';
 
 export default async function createCardsMarkup(cards, element) {
   try {
-    const createMarkup = await cards.hits
-      .map(
-        e => card(e)
-        // `<div class="photo-card">
-        //   <a href="${e.largeImageURL}" class="card-link"><img class="card-img" src="${e.webformatURL}" alt="${e.tags}" loading="lazy" />
-        //   <div class="info">
-        //     <p class="info-item">
-        //       <b>Likes <br>
-        //       ${e.likes}</b>
-        //     </p>
-        //     <p class="info-item">
-        //       <b>Views <br>
-        //       ${e.views}</b>
-        //     </p>
-        //     <p class="info-item">
-        //       <b>Comments <br>
-        //       ${e.comments}</b>
-        //     </p>
-        //     <p class="info-item">
-        //       <b>Downloads <br>
-        //       ${e.downloads}</b>
-        //     </p>
-        //   </div>
-        // </div></a>`
-      )
-      .join('');
+    const createMarkup = await cards.hits.map(e => card(e)).join('');
     const addToDOM = await element.insertAdjacentHTML(
       'beforeend',
       createMarkup
--- src/js/index.js
+++ src/js/index.js
@@ -5,41 +5,47 @@ import createCardsMarkup from './createMarkup';
 import fetchSearch from './fetchSearch';
 import loadMorePages from './loadMorePages';
 import smoothScroll from './smoothScroll';
+const BASE_URL = 'https://pixabay.com/';
 const URL_KEY = '29676323-cbf3b0b0974f66dc50c141bea';
+const failureSearchMessage =
+  'Sorry, there are no images matching your search query. Please try again.';
+export { BASE_URL, URL_KEY, failureSearchMessage };
 let page = 1;
 const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
-  // loadMoreBtn: document.querySelector('.load-more'),
   pageLoadStatus: document.querySelector('.page-load-status'),
 };
 let infScroll = new InfiniteScroll(refs.gallery, {
   path: () =>
-    `https://pixabay.com/api/?key=${URL_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${(page += 1)}`,
+    `${BASE_URL}api/?key=${URL_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${(page += 1)}`,
   history: false,
   scrollThreshold: 100,
 });
 refs.form.addEventListener('submit', async e => {
-  e.preventDefault();
-  clearGallery();
-  PageLoadChangingDisplayStyle();
-  if (!areInputEmpty(e)) return Notiflix.Notify.warning('Type something!');
-
-  page = 1;
-  const fetchResult = await fetchSearch(
-    e.target.elements.searchQuery.value,
-    page
-  );
-  const createdMarkup = await createCardsMarkup(fetchResult, refs.gallery);
-  infScroll.on('scrollThreshold', infScrollOptions);
+  try {
+    e.preventDefault();
+    clearGallery();
+    PageLoadChangingDisplayStyle();
+    const trimmedValue = e.target.elements.searchQuery.value.trim();
+    if (!trimmedValue) return Notiflix.Notify.warning('Type something!');
+    page = 1;
+    const fetchResult = await fetchSearch(trimmedValue, page);
+    const createdMarkup = await createCardsMarkup(fetchResult, refs.gallery);
+    infScroll.on('scrollThreshold', infScrollOptions);
+    Notiflix.Notify.success(`Hooray, we found ${fetchResult.total} images`);
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure(failureSearchMessage);
+  }
 });
 
 function clearGallery() {
   refs.gallery.innerHTML = '';
 }
-function areInputEmpty(e) {
-  return e.target.elements.searchQuery.value.trim();
-}
+// function areInputEmpty(e) {
+//   return e.target.elements.searchQuery.value.trim();
+// }
 async function infScrollOptions() {
   try {
     const fetch = await loadMorePages(
@@ -61,19 +67,3 @@ function PageLoadChangingDisplayStyle(error) {
   }
   refs.pageLoadStatus.style.display = 'none';
 }
-// refs.loadMoreBtn.addEventListener('click', async e => {
-//   page += 1;
-//   const fetchResult = await onButtonMoreClick(
-//     refs.form.elements.searchQuery.value,
-//     page
-//   );
-// const createdMarkup = await createCardsMarkup(fetchResult, refs.gallery);
-// const { height: cardHeight } = document
-//   .querySelector('.gallery')
-//   .firstElementChild.getBoundingClientRect();
-
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: 'smooth',
-// });
-// })
