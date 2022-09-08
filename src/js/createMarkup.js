1997eeb1e57a12c516e37130db9c98fee9e4585e
vitaliy-kro <krochakv6@gmail.com>
2022-09-08 17:18:01 +0300

    changing createMarkup function

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -4,13 +4,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import card from '../template/card.handlebars';
 
 export default async function createCardsMarkup(cards, element) {
-  try {
-    const createMarkup = await cards.hits.map(e => card(e)).join('');
-    const addToDOM = await element.insertAdjacentHTML(
-      'beforeend',
-      createMarkup
-    );
-    const lightbox = new SimpleLightbox('.gallery__item');
-    lightbox.refresh();
-  } catch (error) {}
+  const createMarkup = await cards.hits.map(e => card(e)).join('');
+  const addToDOM = await element.insertAdjacentHTML('beforeend', createMarkup);
+  const lightbox = new SimpleLightbox('.gallery__item');
+  lightbox.refresh();
 }
